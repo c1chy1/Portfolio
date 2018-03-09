2e75307eda668246762e323effa3cd40281128ec
c1chy1 <36567964+c1chy1@users.noreply.github.com>
2018-03-09 16:53:07 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -11,79 +11,76 @@ require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
 
-
-
+$email = $_POST['email'];
+$imie = $_POST['name'];
+$tresc = $_POST['message'];
 
 $mail = new PHPMailer;
+$mail->CharSet = "UTF-8";
 //Tell PHPMailer to use SMTP
 
 // formuła prawidłowego adresu e-mail
-
-
+$mail->SMTPDebug = 2;
 $mailMsg = $_POST['message'];
-$mailto = $_POST ["email"];
 $mail->isSMTP();
-
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 0;
-
-//Ask for HTML-friendly debug output
-$mail->Debugoutput = 'html';
-$mail->CharSet = 'UTF-8'; // the same as 'utf-8'
-//Set the hostname of the mail server
 $mail->Host = 'mail.lima-city.de';
-
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
 $mail->Port = 465;
-
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = "ssl";
-
-//Whether to use SMTP authentication
 $mail->SMTPAuth = true;
-
-//Username to use for SMTP authentication - use full email address for gmail
 $mail->Username = "pawelc@c1chy.de";
-
-//Password to use for SMTP authentication
 $mail->Password = "Coca0808!";
+$mail->SMTPSecure = "ssl";
+$mail->FromName = "c1chy";
+$mail->addAddress('pawelc@c1chy.de','c1chy');
+$mail->addReplyTo($email);
+$mail->isHTML(true);
+$mail->SetFrom('pawelc@c1chy.de','c1chy');
+$mail->Subject = ($imie);
+$mail->Body = ($tresc);
 
-//Set who the message is to be sent from
-$mail->setFrom('pawelc@c1chy.de');
 
-//Set an alternative reply-to address
-$mail->addReplyTo('pawelc@c1chy.de');
 
-//Set who the message is to be sent to
-$mail->AddAddress('pawelc@c1chy.de');
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
 
+
+if($imie == "" OR $email == "" OR $tresc == ""){
+    echo("<script>location.href = 'graphic/validation.png';</script>");
+    exit;
+}
+
+
+
+
+if(!$mail->validateAddress($email)){
+    echo("<script>location.href = 'graphic/Wrong-email-address.jpg';</script>");
+    exit;
+}
+ else
+ {
+     echo("<script>location.href = 'thx.html';</script>");
+ }
 
 
 //Set the subject line
-$mail->Body = $mailMsg;
-$mail->Subject = $mailto;
-$mail->IsHTML(true);
-
-if(!$mail->Send()){
 
 
-
-    echo("<script>location.href = 'thx.html';</script>");
+if(!$mail->Send()) {
+    echo 'Some error... / Jakiś błąd...';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+    exit;
 }
 
-else {
-    echo("<script>location.href = 'thx.html';</script>");
-
-
-}
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-
+echo 'Message has been sent (OK) / Wiadomość wysłana (OK)';
 
 ?>
+
+
+
 
 
 
